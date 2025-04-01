fff3c2a6bea52b7a0b76e3c47982078ae3dad230
Rupam Sarkar <161940237+Rupamsarkar123@users.noreply.github.com>
2025-04-01 14:25:24 +0530

    Update server.js

--- back/server.js
+++ back/server.js
@@ -6,7 +6,14 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+
+const corsOptions = {
+  origin: "https://rupamsportfolio12.vercel.app", // Allow requests from your frontend
+  methods: "GET,POST",
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
 // MySQL Connection
@@ -44,26 +51,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
-
-// import mysql from "mysql2";
-
-// // 1: To connect to MySQL server
-// const db = await mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "Rupam123@",
-//   // database: "",
-// });
-
-// console.log("MySQL Connected Successfully :)");
-
-// db.query("SHOW DATABASES", (err, results) => {
-//   if (err) {
-//     console.error("Error fetching databases:", err);
-//     return;
-//   }
-//   console.log("Available Databases:", results);
-// });
-// // 2: Create a database (DB)
-// // 3: Create a table
-// // 4: Perform CRUD operations
