4a569acdc2cc03933733ef5cc90566eca04cdcfa
Rupam Sarkar <161940237+Rupamsarkar123@users.noreply.github.com>
2025-04-21 04:14:21 +0530

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -22,7 +22,8 @@ export const Contact = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:5000/api/contact", {
+      const response = await fetch("https://portfolio-2-yp4y.onrender.com/api/contact", {
+
         method: "POST",
         headers: {
           "Content-Type": "application/json",
